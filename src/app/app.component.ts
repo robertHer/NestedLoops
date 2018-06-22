b1b2006f6bc9684238e82e3795c3e72ab6f2db5e
robertHer <totolero769@gmail.com>
2018-06-21 17:53:23 -0700

    nesting loops

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  bools:boolean[] = [true,false];
+  nums:number[] = [1,2.5,5];
+  strs:string[] = ['hi', 'there'];
+  test :Array<{id:number, name:string}> = [{
+    id:1 ,
+    name: 'test'
+  }];
+  user:Array<{id:number, name:string}> = [{
+    id:2 ,
+    name: 'user'
+  }];
+  objs:Array<any>  = [
+    this.test, this.user
+  ];
+  types:any[] = [this.bools,this.nums,this.strs,this.objs];
+  
+  ngOnInit(){
+    
+    console.log(this.objs)
+  }
+
+
+  getTypeof(type:string){
+    
+   let x = typeof type;
+    console.log(typeof x);
+    return typeof x;
+  }
 }
